7a45ca81136e3b26929ca62e9e6c10aae2b8e8c9
kallaugher <alicekallaugher@gmail.com>
2016-09-22 14:29:45 -0400

    create simple view using createElement()

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Code React element here
+const title = React.createElement('h1', {}, 'An Awesome Person');
+const para = React.createElement('p', {}, 'Who is learning React' )
+
+const list =
+  React.createElement('ul', { className: 'me__interests' },
+    [
+      React.createElement('li', {}, 'JavaScript'),
+      React.createElement('li', {}, 'React'),
+      React.createElement('li', {}, 'Movies'),
+      React.createElement('li', {}, 'Ice cream')
+    ])
+
+const meInReact = React.createElement('div', { className: 'me'}, [title, para, list])
+
+ReactDOM.render(meInReact, document.getElementById('main'));
